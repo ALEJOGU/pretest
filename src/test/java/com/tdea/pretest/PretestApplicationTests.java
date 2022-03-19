2b5c4636dc2fded78000b7c0987e79eda6c81866
ALEJOGU <99086790+ALEJOGU@users.noreply.github.com>
2022-03-19 11:43:20 -0500

    Update PretestApplicationTests.java
    Password Equals

--- src/test/java/com/tdea/pretest/PretestApplicationTests.java
+++ src/test/java/com/tdea/pretest/PretestApplicationTests.java
@@ -8,8 +8,8 @@ class PretestApplicationTests {
 
     @Test
     void isPasswordEqual() {
-        String dbPassword = "yeah";
-        String password = "yeah";
+        String dbPassword = "yeah_1s2s3";
+        String password = "yeah_1s2s3";
         UserUtils pass_equal = new UserUtils();
         assertTrue(pass_equal.isPasswordEqual(dbPassword, password));
 
